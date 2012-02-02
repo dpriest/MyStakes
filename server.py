c46d2e1a1d478e8e8894a294972fe07040e5e6da
Randy Lu <djyde520@gmail.com>
2012-02-02 16:00:40 +0800

    server

--- server.py
+++ server.py
@@ -1,19 +1,9 @@
 #! usr/bin/env python
 # coding: utf-8
-
 import web
-
-urls=(
-	"/","index",
-)
-
-render=web.template.render("templates")
+from config.url import urls
 
 app=web.application(urls,globals())
-
-class index:
-	def GET(self):
-		return render.index()
 
 if __name__=="__main__":
 	app.run()
